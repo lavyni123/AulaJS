70d7a4e6b1ec967e19399981639a64b09dde845c
lavyni123 <lavynisantana019@gmail.com>
2026-02-24 11:37:14 -0300

    24/02/2026

---
+++ Aula01/exemplo1.js
@@ -0,0 +1,13 @@
+document.write ("<hr> Entrada de dados</h2>")
+
+var numero1 = prompt ("digite o numero 1");
+var numero2 = prompt("digite o numero 2");
+
+document.write("numero 1 = " + numero1 + "<br>");
+document.write("numero 2 = " + numero2 + "<br>");
+
+document.write ("<h2> Operações Aritmeticas</h2>");
+var resultado;
+
+resultado= Number (numero1) + Number(numero2);
+document.write("A soma dos numeros é " + resultado + "<br>");
---
+++ Aula01/scrip.js
@@ -0,0 +1,14 @@
+var nome = "lavyni";
+var  idade = 46;
+var altura = 1.82;
+console.log( nome);
+console.log( idade);
+console.log(altura);
+document.write("<h1> Ola, seja bem vindo ao JavaScript</h1>")
+document.write( nome + "<br>");
+document.write( idade + "<br>");
+document.write( altura + "<br>");
+
+var dias = idade * 365;
+
+document.write("<p style= 'color : red'>Você já viveu" + dias + "dias</p>")
