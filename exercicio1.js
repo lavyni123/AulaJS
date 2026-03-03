dbfa5589b63b764c42ab8ce227fb3402b6ec6662
lavyni123 <lavynisantana019@gmail.com>
2026-03-03 11:38:23 -0300

    03/03/2026

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+document.write("<h1> aula 01 - exercicio 1</h1>");
+
+var numero = prompt("digite um numero qualquer:");
+
+var quadrado = Number(numero) * Number(numero);
+
+var cubo = Number(numero) * Number(numero);
+
+document.write(" <p> O numero digitado foi" + numero + "</p>");
+
+document.write("<p style= 'color: red'> O quadrado do numero é " + quadrado + "</p>")
+document.write("<p style= 'color: red'> O cubo do numero é " + cubo + "</p>")
+
+
---
+++ exercicio2.js
@@ -0,0 +1,6 @@
+document.write("<h1> aula 01 - exercicio 2</h1>");
+
+var numero = prompt("digite um numero qualquer:");
+var celsius =  ( Number(numero-32)*5) / 9;
+
+document.write("<p style= 'color: red'> O celsius do numero é " + celsius + "</p>")
