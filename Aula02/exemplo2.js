18af4cab00ab99e1367956715fafc066d6b34761
lavyni123 <lavynisantana019@gmail.com>
2026-03-24 11:39:48 -0300

    24/03/26

---
+++ Aula02/exemplo2.js
@@ -0,0 +1,10 @@
+function Exemplo2()
+{
+    let numero = Number(document.geteElementByld ("numero" ) .value );
+    let quadrado,cubo;
+    quadrado= numero * numero;
+    cubo= numero * numero * numero;
+
+    document.getElementByld("resultado").innerHTML =
+    "<p> O quadrado do numero é" + quadrado + "e o cubo é" + cubo + "</p>";
+}
---
+++ Aula02/exercicio1.js
@@ -0,0 +1,6 @@
+document.write("<h1> aula 02 - exercicio 1</h1>");
+
+var numero = prompt("digite um numero qualquer:");
+var celsius =  ( Number(numero-32)*5) / 9;
+
+document.write("<p style= 'color: red'> O celsius do numero é " + celsius + "</p>")
